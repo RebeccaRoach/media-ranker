46836f0bdf2cbee736caa72ff4ac196527c77866
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-17 17:56:28 -0700

    Added Vote model and made associations with User and Work.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,10 @@ class ApplicationController < ActionController::Base
   def record_not_found
     render file: "#{Rails.root}/public/404.html" , status: 404
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+  # helper view method that makes current_user method available in view
+  helper_method :current_user
 end
