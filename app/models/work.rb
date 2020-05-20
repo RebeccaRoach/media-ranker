2f8854e911e2c6fb061171dd840efb02a76119a9
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-19 19:33:21 -0700

    Made _form partial to display 3 media sections in works index, implemented get_category method in Work model.

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,4 +2,11 @@ class Work < ApplicationRecord
   has_many :votes
 
   # has_many :users, through: :votes
+
+  # class method to sort by max votes
+
+  def self.get_category(category_name)
+    return self.where(category: category_name)
+  end
+
 end
