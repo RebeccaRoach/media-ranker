89a70b82d4e3dd60bee499c99c5e481c67e0e79f
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-20 22:58:14 -0700

    Implemented self#spotlight and #most_recent_vote_date methods in work model to find spotlight media.

--- app/models/work.rb
+++ app/models/work.rb
@@ -17,4 +17,44 @@ class Work < ApplicationRecord
     }
   end
 
+
+  def most_recent_vote_date
+    # returns most recent created_at date of votes
+    votes = self.votes
+    most_recent_date = Date.new(1965,5,20)
+
+    votes.each do |vote|
+      if vote.created_at > most_recent_date
+        most_recent_date = vote.created_at
+      end
+    end
+
+    return most_recent_date
+  end
+
+
+  def self.spotlight(category_name)
+    # return single top work based on number of votes
+    # or for tie, return the most recently upvoted work
+    top_10 = self.top_ten(category_name)
+
+    max_votes = 0
+    ties = []
+
+    top_10.each do |work|
+      if work.votes.count == max_votes
+        ties << work
+      elsif work.votes.count > max_votes
+        ties = [work]
+        max_votes = work.votes.count
+      end
+    end
+
+    if ties.length > 1
+      return ties.max_by {|work| work.most_recent_vote_date }
+    else
+      return ties[0]
+    end
+
+  end
 end
