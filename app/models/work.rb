b755b7e54e11355b05b89462128e5d0c3683d1f6
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-20 21:38:22 -0700

    Implemented top_ten method and added partial for media displays.

--- app/models/work.rb
+++ app/models/work.rb
@@ -9,4 +9,12 @@ class Work < ApplicationRecord
     return self.where(category: category_name)
   end
 
+  def self.top_ten(category_name)
+    # find the top 10 works in a category that have the most descending votes
+    # any tie breaking logic required??
+    return self.get_category(category_name).max_by(10) {
+      |work| work.votes.count
+    }
+  end
+
 end
