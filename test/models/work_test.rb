978e4a5136ca3e47eb7ef3cf93113e25fa479952
RebeccaRoach <rebeccaroach12@gmail.com>
2020-05-23 16:52:57 -0700

    Validations tests on work passing.

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
 
 describe Work do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  # 1+ test for each validation (all pass, some pass, none pass)
+  #     validates title: presence: true
+  # 1+ test for each relation (has many votes)
+  # 1+ test for each custom method on work model
+
+  describe 'validations' do
+    before do
+      @book1 = works(:book_1)
+    end
+
+    it 'is valid when all fields are present' do
+      result = @book1.valid?
+
+      expect(result).must_equal true
+    end
+
+    it 'is valid when not every field is present' do
+
+      some_field_params = {
+        category: 'book',
+        title: 'A book with some title',
+        publication_year: 1995
+      }
+
+      book_some_fields = Work.new(some_field_params)
+
+      expect(book_some_fields.valid?).must_equal true
+    end
+
+    it 'is invalid without a title' do
+      @book1.title = nil
+      
+      expect(@book1.valid?).must_equal false
+      expect(@book1.errors.messages).must_include :title
+    end
+  end
+
+  describe 'relations' do
+    it 'can have many votes' do
+
+      # create 3 votes for this work_id
+      # expect that the length of the array to be 3
+        # (Work.votes).must_equal 3 
+        # each of the three votes must be kind of vote
+
+
+
+   
+    end
+  end
 end
